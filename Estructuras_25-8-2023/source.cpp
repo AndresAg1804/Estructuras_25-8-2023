9b360f03a29c358889499eba137d85cb8c323673
Escuela de Informática UNA <ustds_escinf@hotmail.com>
2023-08-25 21:33:48 -0600

    nuevo

--- Estructuras_25-8-2023/source.cpp
+++ Estructuras_25-8-2023/source.cpp
@@ -1,30 +1,74 @@
 #include <iostream>
-
-int global = 19;
-int f1(int, char);
-void f2();
+#include <vector>
 
 int main() {
-	int x = 10;
-	int y = 20;
-	int sum;
-	char caracter = 'L';
+	std::vector<int> v;
+	
+	v.push_back(10);
+	v.push_back(18);
+	v.push_back(9);
+	v.push_back(5);
+	//v.assign(10);
+	
+	std::vector<int>::iterator iter;
 
-	sum = x + y;
-	f1(sum, caracter);
+	for (auto i : v)
+	{
+		std::cout << i << "\n";
+	}
+	
+	v.clear();
 
-	sum += x;
-
-	f2();
 	return 0;
 }
 
-int f1(int a, char c) {
-	std::cout << a << "\n";
-	std::cout << c << "\n";
-	return 0;
-}
 
-void f2() {
-	std::cout << "Cerveza despues de clases";
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//int global = 19;
+//int f1(int, char);
+//void f2();
+//
+//int main() {
+//	int x = 10;
+//	int y = 20;
+//	int sum;
+//	char caracter = 'L';
+//
+//	sum = x + y;
+//	f1(sum, caracter);
+//
+//	sum += x;
+//
+//	f2();
+//	return 0;
+//}
+//
+//int f1(int a, char c) {
+//	std::cout << a << "\n";
+//	std::cout << c << "\n";
+//	return 0;
+//}
+//
+//void f2() {
+//	std::cout << "Cerveza despues de clases";
+//}
